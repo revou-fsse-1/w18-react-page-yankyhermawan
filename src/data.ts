ed3474861a9a4ad9045907d92c7cec1201a1735a
yankyhermawan <yankyhermawan42@gmail.com>
2023-06-06 21:52:33 +0700

    initial commit

---
+++ src/data.ts
@@ -0,0 +1,50 @@
+export const Data=[
+    {
+        id: 1,
+        name: "The Trails",
+        isLiked: false,
+        url: "https://images.pexels.com/photos/13835530/pexels-photo-13835530.jpeg?auto=compress&cs=tinysrgb&w=800"
+    },
+    {
+        id:2,
+        name: "The River",
+        isLiked: false,
+        url: "https://images.pexels.com/photos/15202881/pexels-photo-15202881.jpeg?auto=compress&cs=tinysrgb&w=800"
+    },
+    {
+        id:3,
+        name: "The Lakeside",
+        isLiked: false,
+        url: "https://images.pexels.com/photos/11389480/pexels-photo-11389480.jpeg?auto=compress&cs=tinysrgb&w=800"
+    },
+    {
+        id:4,
+        name: "The Hike",
+        isLiked: false,
+        url: "https://images.pexels.com/photos/13172865/pexels-photo-13172865.jpeg?auto=compress&cs=tinysrgb&w=800"
+    },
+    {
+        id:5,
+        name: "The Chill Hill",
+        isLiked: false,
+        url: "https://images.pexels.com/photos/1624438/pexels-photo-1624438.jpeg?auto=compress&cs=tinysrgb&w=800"
+    },
+    {
+        id:6,
+        name: "The Bridge",
+        isLiked: false,
+        url: "https://images.pexels.com/photos/1761279/pexels-photo-1761279.jpeg?auto=compress&cs=tinysrgb&w=800"
+    },
+    {
+        id:7,
+        name: "The Autumn",
+        isLiked: false,
+        url: "https://images.pexels.com/photos/1808329/pexels-photo-1808329.jpeg?auto=compress&cs=tinysrgb&w=800"
+    },
+    {
+        id:8,
+        name: "The Cliff",
+        isLiked: false,
+        url: "https://images.pexels.com/photos/1028225/pexels-photo-1028225.jpeg?auto=compress&cs=tinysrgb&w=800"
+    },
+]
